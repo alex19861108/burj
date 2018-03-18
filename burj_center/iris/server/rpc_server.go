54e4e7bcf69c04cc2b9ac3b3d917eab940e25cbb
alex1986 <alex19861108@live.cn>
2018-03-18 10:14:53 +0800

    add get node and get devices

--- burj_center/iris/server/rpc_server.go
+++ burj_center/iris/server/rpc_server.go
@@ -32,6 +32,25 @@ func (s *RpcServer) Search(ctx context.Context, req *proto.SearchNodeRequest) (*
 	}, nil
 }
 
+func (s *RpcServer) GetNodes(ctx context.Context, in *proto.GetNodesRequest) (*proto.GetNodesResponse, error) {
+	nodes, _ := s.NodeService.GetAll()
+	return &proto.GetNodesResponse{
+		Nodes: nodes,
+	}, nil
+}
+func (s *RpcServer) GetDevices(ctx context.Context, in *proto.GetDevicesRequest) (*proto.GetDevicesResponse, error) {
+	var devices []*proto.Device
+	nodes, _ := s.NodeService.GetAll()
+	for _, node := range nodes {
+		for _, device := range node.Devices {
+			devices = append(devices, device)
+		}
+	}
+	return &proto.GetDevicesResponse{
+		Devices: devices,
+	}, nil
+}
+
 func InitRpcServer(cfg config.Config, service services.NodeService) {
 	listener, err := net.Listen("tcp", ":"+cfg.ServerConfig.Port.Rpc)
 	if err != nil {
